354e59bce5608ca60f56f9f481ca2bcd60d42a8e
Hai Hoang <haihoang@Hais-MacBook-Pro.local>
2015-03-08 16:26:12 -0700

    added csvtojson parser

--- data/converter.js
+++ data/converter.js
@@ -1,3 +1,10 @@
 /**
  * Created by haihoang on 2015-03-08.
- */
+ *
+ *
+ * Converter class to parse .csv file and convert to JSON
+ *
+ * Parser source code: https://github.com/Keyang/node-csvtojson
+ * /
+
+
