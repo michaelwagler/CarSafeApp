0ea36d10a3202ff112a09e4614a80969452657a0
Hai Hoang <haihoang@Hais-MacBook-Pro.local>
2015-03-08 19:31:27 -0700

    basic functional parser, parser manager not working

--- data/converter.js
+++ data/converter.js
@@ -8,20 +8,31 @@
  */
 
 
- //Converter Class
- var Converter=require("csvtojson").core.Converter;
- var fs=require("fs");
+////Parser Manager
+//var parserMgr=require("csvtojson").core.parserMgr;
+//
+//parserMgr.addParser("carSafeParser",/^\*TYPE\*/,function (params){
+//    var columnTitle=params.head; //params.head be like: *parserRegExp*ColumnName;
+//    var fieldName=columnTitle.replace(this.regExp, ""); //this.regExp is the regular expression above.
+//    params.resultRow[fieldName]="Hello my parser"+params.item;
+//});
 
- var csvFileName="./data/crime_2014.csv";
- var fileStream=fs.createReadStream(csvFileName);
- //new converter instance
- var param={};
- var csvConverter=new Converter(param);
 
- //end_parsed will be emitted once parsing finished
- csvConverter.on("end_parsed",function(jsonObj){
+
+//Converter Class
+var Converter=require("csvtojson").core.Converter;
+var fs=require("fs");
+
+var csvFileName="./crime_2014.csv";
+var fileStream=fs.createReadStream(csvFileName);
+//new converter instance
+var param={};
+var csvConverter=new Converter(param);
+
+//end_parsed will be emitted once parsing finished
+csvConverter.on("end_parsed",function(jsonObj){
    console.log(jsonObj); //here is your result json object
 });
 
- //read from file
- fileStream.pipe(csvConverter);
+//read from file
+fileStream.pipe(csvConverter);
