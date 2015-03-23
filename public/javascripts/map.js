8ef4d4a057b930b7b48f8dad24e45800c81e9534
Hai Hoang <hai.manh.hoang@gmail.com>
2015-03-23 16:04:51 -0700

    centered map around Vancouver

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,8 +4,8 @@
 var map;
 function initialize() {
     var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644)
+        zoom: 11,
+        center: new google.maps.LatLng(49.246292, -123.116226)
     };
     map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
